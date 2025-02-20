daf06fc3e96ae61261b00c13fcba9b4429d034fa
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-20 13:49:01 +0200

    Create User.cs

---
+++ ProjectHive.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace ProjectHive.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
