ed56ef79ce5f4e4413fba85336117097c38bf893
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-20 13:47:58 +0200

    Create Project.cs

---
+++ ProjectHive.Domain/Entities/Project.cs
@@ -0,0 +1,9 @@
+namespace ProjectHive.Domain.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
