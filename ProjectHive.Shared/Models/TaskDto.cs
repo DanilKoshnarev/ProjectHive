2cda4ccb06f13dfe9a6a8b96bfc1554d5e47b043
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-20 13:58:58 +0200

    Create TaskDto.cs

---
+++ ProjectHive.Shared/Models/TaskDto.cs
@@ -0,0 +1,11 @@
+namespace ProjectHive.Shared.Models
+{
+    public class TaskDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
