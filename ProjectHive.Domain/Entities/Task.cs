76f4ff0bb59fec4ec69a5dcf75217317e9f9c994
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-20 13:47:21 +0200

    Create Task.cs

---
+++ ProjectHive.Domain/Entities/Task.cs
@@ -0,0 +1,11 @@
+namespace ProjectHive.Domain.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int ProjectId { get; set; }
+    }
+}
